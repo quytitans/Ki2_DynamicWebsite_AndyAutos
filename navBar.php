e7330c2412ca6a455bfc9e251901939afc67c1c3
QuyNguyen <quynv.mr@gmail.com>
2021-11-06 02:55:05 +0700

    hehe

--- navBar.php
+++ navBar.php
@@ -1,10 +1,10 @@
 <h1>Andy's Autos</h1>
 <div class="topnav row" id="myTopnav">
     <div class="col-md-12 col-lg-3">
-        <a href="/Ki2_DynamicWebsite_AndyAutos/home.php">Home</a>
+        <a href="https://andyauto.herokuapp.com/">Home</a>
     </div>
     <div class="col-md-12 col-lg-3">
-        <a href="/Ki2_DynamicWebsite_AndyAutos/contactUs.php">Contact Us</a>
+        <a href="https://andyauto.herokuapp.com/contactUs.php">Contact Us</a>
     </div>
     <div class="col-md-12 col-lg-3">
         <a href="/Ki2_DynamicWebsite_AndyAutos/aboutUs.php">Google Analytics</a>
