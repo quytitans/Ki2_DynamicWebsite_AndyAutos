3a76a3da8bc5aa86950b4b8997c8897c6efa0a6c
QuyNguyen <quynv.mr@gmail.com>
2021-11-06 02:09:10 +0700

    hello

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./library/bootstrap.css">
+    <?php include 'library.php'; ?>
+    <link rel="stylesheet" href="./css/home.css">
+    <title>Home Page</title>
+</head>
+<body class="container">
+<?php include 'navBar.php'; ?>
+<div style="padding-left:16px" class="row">
+    <div class="col-12 col-lg-12">
+        <h2>Sản phẩm bán chạy</h2>
+    </div>
+    <div class="col-12 col-lg-3 mt-4">
+        <img class="w-100 rounded"
+             src="https://res.cloudinary.com/quynv300192/image/upload/v1636096206/gia-xe-bmw-x6-oto-com-vn-1-ae52_ythtoh.jpg"
+             alt="">
+        <h3 class="align-items-center">BMW X6</h3>
+        <h4>Price: 4000 USD</h4>
+        <button class="btn-success">Add to cart</button>
+    </div>
+    <div class="col-12 col-lg-3 mt-4">
+        <img class="w-100 rounded"
+             src="https://res.cloudinary.com/quynv300192/image/upload/v1636096206/gia-xe-bmw-x6-oto-com-vn-1-ae52_ythtoh.jpg"
+             alt="">
+        <h3 class="align-items-center">BMW X6</h3>
+        <h4>Price: 4000 USD</h4>
+        <button class="btn-success">Add to cart</button>
+    </div>
+    <div class="col-12 col-lg-3 mt-4">
+        <img class="w-100 rounded"
+             src="https://res.cloudinary.com/quynv300192/image/upload/v1636096206/gia-xe-bmw-x6-oto-com-vn-1-ae52_ythtoh.jpg"
+             alt="">
+        <h3 class="align-items-center">BMW X6</h3>
+        <h4>Price: 4000 USD</h4>
+        <button class="btn-success">Add to cart</button>
+    </div>
+    <div class="col-12 col-lg-3 mt-4">
+        <img class="w-100 rounded"
+             src="https://res.cloudinary.com/quynv300192/image/upload/v1636096206/gia-xe-bmw-x6-oto-com-vn-1-ae52_ythtoh.jpg"
+             alt="">
+        <h3 class="align-items-center">BMW X6</h3>
+        <h4>Price: 4000 USD</h4>
+        <button class="btn-success">Add to cart</button>
+    </div>
+    <div class="col-12 col-lg-3 mt-4">
+        <img class="w-100 rounded"
+             src="https://res.cloudinary.com/quynv300192/image/upload/v1636096206/gia-xe-bmw-x6-oto-com-vn-1-ae52_ythtoh.jpg"
+             alt="">
+        <h3 class="align-items-center">BMW X6</h3>
+        <h4>Price: 4000 USD</h4>
+        <button class="btn-success">Add to cart</button>
+    </div>
+    <div class="col-12 col-lg-3 mt-4">
+        <img class="w-100 rounded"
+             src="https://res.cloudinary.com/quynv300192/image/upload/v1636096206/gia-xe-bmw-x6-oto-com-vn-1-ae52_ythtoh.jpg"
+             alt="">
+        <h3 class="align-items-center">BMW X6</h3>
+        <h4>Price: 4000 USD</h4>
+        <button class="btn-success">Add to cart</button>
+    </div>
+    <div class="col-12 col-lg-3 mt-4">
+        <img class="w-100 rounded"
+             src="https://res.cloudinary.com/quynv300192/image/upload/v1636096206/gia-xe-bmw-x6-oto-com-vn-1-ae52_ythtoh.jpg"
+             alt="">
+        <h3 class="align-items-center">BMW X6</h3>
+        <h4>Price: 4000 USD</h4>
+        <button class="btn-success">Add to cart</button>
+    </div>
+    <div class="col-12 col-lg-3 mt-4">
+        <img class="w-100 rounded"
+             src="https://res.cloudinary.com/quynv300192/image/upload/v1636096206/gia-xe-bmw-x6-oto-com-vn-1-ae52_ythtoh.jpg"
+             alt="">
+        <h3 class="align-items-center">BMW X6</h3>
+        <h4>Price: 4000 USD</h4>
+        <button class="btn-success">Add to cart</button>
+    </div>
+    <div class="col-12 col-lg-3 mt-4">
+        <img class="w-100 rounded"
+             src="https://res.cloudinary.com/quynv300192/image/upload/v1636096206/gia-xe-bmw-x6-oto-com-vn-1-ae52_ythtoh.jpg"
+             alt="">
+        <h3 class="align-items-center">BMW X6</h3>
+        <h4>Price: 4000 USD</h4>
+        <button class="btn-success">Add to cart</button>
+    </div>
+    <div class="col-12 col-lg-3 mt-4">
+        <img class="w-100 rounded"
+             src="https://res.cloudinary.com/quynv300192/image/upload/v1636096206/gia-xe-bmw-x6-oto-com-vn-1-ae52_ythtoh.jpg"
+             alt="">
+        <h3 class="align-items-center">BMW X6</h3>
+        <h4>Price: 4000 USD</h4>
+        <button class="btn-success">Add to cart</button>
+    </div>
+</div>
+<a href="http://www.heroku.com" target="_blank" class="btn">Heroku.com</a>
+</body>
+<div class="row footer1">
+    <h5>&copy;Copy right 2021 NVQ</h5>
+</div>
+</html>
